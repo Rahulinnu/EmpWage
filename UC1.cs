5dbc49a185d8f26117eb0e1a17f0bdaae1086bea
rahul unni <rahul@rahulsmac.local>
2022-05-01 20:43:27 +0530

    UC1 added

---
+++ UC1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace EmpWage
+{
+    public class UC1
+    {
+        public UC1()
+        {
+            int empPresent = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == empPresent) 
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
