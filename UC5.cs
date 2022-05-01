abe846e2a456af5dc7180f749a6240ab75fc45bd
rahul unni <rahul@rahulsmac.local>
2022-05-01 21:37:57 +0530

    UC5 added

---
+++ UC5.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace EmpWage
+{
+    public class UC5
+    {
+        public UC5()
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            int wage_Per_Hour = 20;
+            int totWage = 0;
+            Random random = new Random();
+            for (int i = 1; i <= 20; i++)
+            {
+                int empCheck = random.Next(0, 3);
+
+
+                switch (empCheck)
+                {
+                    case 0:
+                        Console.WriteLine("Employee is Full time");
+                        empHrs = 8;
+                        break;
+
+                    case 1:
+                        Console.WriteLine("Employee is Part time");
+                        empHrs = 4;
+                        break;
+
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        empHrs = 0;
+                        break;
+
+                }
+
+                empWage = empHrs * wage_Per_Hour;
+                Console.WriteLine("Employee wage = " + empWage);
+                totWage = totWage + empWage;
+            }
+
+            Console.WriteLine("Total wage = " + totWage);
+        
+        }
+    }
+}
